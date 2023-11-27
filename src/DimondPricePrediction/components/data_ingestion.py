a113a04bd7d67efe5ec09bc74811cc052e7be8cb
sravankavati <sravankavati119@gmail.com>
2023-11-27 16:37:40 +0530

    data transformation

--- src/DimondPricePrediction/components/data_ingestion.py
+++ src/DimondPricePrediction/components/data_ingestion.py
@@ -42,9 +42,17 @@ class DataIngestion:
             test_data.to_csv(self.ingestion_config.test_data_path,index=False)
             
             logging.info("data ingestion part completed")
+
+
+            return (
+                 
+                
+                self.ingestion_config.train_data_path,
+                self.ingestion_config.test_data_path
+            )
             
             
-        
+            
         except Exception as e:
            logging.info("exception during occured at data ingestion stage")
            raise customexception(e,sys)
